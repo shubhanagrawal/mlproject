9aefb6ef7a3c6f88bd24c1b84a7e90266f53bfe4
shubhanagrawal <shubhanagrawal413@gmail.com>
2025-05-09 20:42:43 +0530

    Prediction Pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -98,7 +98,7 @@ class ModelTrainer:
             best_model = models[best_model_name]
 
             if best_model_score<0.6:
-                raise CustomException("No best model found")
+                raise Exception("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
@@ -116,4 +116,4 @@ class ModelTrainer:
 
             
         except Exception as e:
-            raise CustomException(e,sys)
+            raise Exception(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,11 @@
 import os
-import sys
 import numpy as np
 import pandas as pd
-import os
 import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import pickle
+
 
 def save_object(file_path, obj):
     try:
@@ -19,38 +19,40 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise Exception(f"Error saving object to {file_path}: {e}")
-    
 
 
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
+        for i in range(len(models)):
+            model_name = list(models.keys())[i]
+            model = models[model_name]
+            params = param[model_name]
 
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
+            gs = GridSearchCV(model, params, cv=3)
+            gs.fit(X_train, y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
-
-            #model.fit(X_train, y_train)  # Train model
+            model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
 
         return report
 
-
     except Exception as e:
-    
         raise Exception(f"Error evaluating models: {e}")
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise Exception(f"Error loading object from {file_path}: {e}")
